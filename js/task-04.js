ba250491ca8d4095689a0a4552080f0db2674ba0
Mateusz Kubiak <smuku.mk@gmail.com>
2023-05-03 17:55:25 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,16 @@
+let counterValue = 0;
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-const counterValue = 0;
+const value1 = document.querySelector("#value");
 
 const decrement = () => {
   counterValue--;
+  value1.textContent = counterValue;
 };
 const increment = () => {
   counterValue++;
+  value1.textContent = counterValue;
 };
 
 decrementBtn.addEventListener("click", decrement);
 incrementBtn.addEventListener("click", increment);
-
-const value = document.querySelector("#value");
-value.value = counterValue;
