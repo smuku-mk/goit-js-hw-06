48e279fdfb086a47c3c6ec1a8b509638116cdd3b
Mateusz Kubiak <smuku.mk@gmail.com>
2023-05-07 18:00:57 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+function checkSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Wszystkie pola powinny zostać wypełnione");
+  }
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
+}
+
+formEl.addEventListener("submit", checkSubmit);
